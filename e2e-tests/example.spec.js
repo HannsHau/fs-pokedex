c8687cd45743fdc710325ff909891a65082b316d
HannsHau <bierkoenig@web.de>
2026-04-07 15:56:20 +0200

    add link test

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -7,3 +7,16 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
+
+describe('Check Link ivysaur and watch out for chlorophyll', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await page.getByText('ivysaur').click();
+    await page.waitForURL('**/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+
+
+    
+  })
+})
