ff603fb280f78f92c93b77eb98ae694c8b4a2f55
HannsHau <bierkoenig@web.de>
2026-04-08 15:46:54 +0200

    failure

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('6.1 should not work, really') // change this string to ensure a new version deployed
+  res.send('6.2 failure'); // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
